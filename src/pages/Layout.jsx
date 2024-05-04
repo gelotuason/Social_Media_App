add116871ea69d7acbd042740fba628b1a478431
gelotuason <gelotuason@gmail.com>
2024-05-04 21:31:23 +0800

    Updated layout component

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 function Layout() {
     return (
-        <Outlet />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Outlet />
+        </LocalizationProvider>
     )
 }
 
