ede80c6fc165abf6736c184ca0f141e519ffa51d
gelotuason <gelotuason@gmail.com>
2024-05-06 00:04:44 +0800

    added post functionality (partial)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, addDoc, collection, Timestamp, onSnapshot } from 'firebase/firestore';
 import firebaseApp from '../config/firebaseConfig'
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
@@ -9,13 +10,16 @@ import Grid from '@mui/material/Grid';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Icon from '@mui/material/Icon';
+import Post from './Post';
 
 function Home() {
 
     const [userProfile, setUserProfile] = useState({});
-
+    const [body, setBody] = useState('');
+    const [posts, setPosts] = useState([]);
+    
     const auth = getAuth(firebaseApp);
+    const db = getFirestore(firebaseApp);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -25,10 +29,8 @@ function Home() {
                 // https://firebase.google.com/docs/reference/js/auth.user
                 const uid = user.uid;
                 // ...
-                console.log(user);
-
                 setUserProfile({
-                    displayName: user.displayName,
+                    name: user.displayName,
                     email: user.email
                 });
             } else {
@@ -37,7 +39,34 @@ function Home() {
                 navigate('/');
             }
         });
+
+        onSnapshot(collection(db, "posts"), snapshot => {
+            setPosts(snapshot.docs.map((post) => (
+                post.data()
+            )));
+        });
     }, []);
+
+    const handleShare = () => {
+        // Post data - avatar, name, date, body
+
+        const postData = {
+            avatar: 'https://cdn.nba.com/headshots/nba/latest/1040x760/445.png',
+            name: userProfile.name,
+            body: body,
+            files: '',
+            date: '',
+        }
+
+        try {
+            addDoc(collection(db, "posts"), postData).then(() => {
+                setBody('');
+            });
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
 
     return (
         <Container maxWidth='lg'>
@@ -45,9 +74,9 @@ function Home() {
                 <Grid container>
                     <Grid item xs={12} padding={'16px'}>
                         <Grid item xs={12} sx={{ display: 'flex' }}>
-                            <Avatar sx={{}} src='https://cdn.nba.com/headshots/nba/latest/1040x760/445.png' />
+                            <Avatar src='https://cdn.nba.com/headshots/nba/latest/1040x760/445.png' />
                             <Box sx={{ display: 'flex', flexDirection: 'column', marginLeft: '16px' }}>
-                                <Typography variant='subtitle1'>{userProfile.displayName}</Typography>
+                                <Typography variant='subtitle1'>{userProfile.name}</Typography>
                                 <Typography variant='subtitle2'>{userProfile.email}</Typography>
                             </Box>
                         </Grid>
@@ -62,6 +91,12 @@ function Home() {
                                 <Avatar sx={{ marginTop: '8px' }} src='https://cdn.nba.com/headshots/nba/latest/1040x760/445.png' />
                                 <Box sx={{ display: 'flex', flexDirection: 'column', marginLeft: '24px', width: '100%' }}>
                                     <TextField
+                                        onChange={(e) => {
+                                            setBody(
+                                                e.target.value
+                                            )
+                                        }}
+                                        value={body}
                                         multiline
                                         placeholder="Share your thoughts.."
                                         maxRows={4}
@@ -76,18 +111,27 @@ function Home() {
                                     >
                                         <Button size="small" sx={{ marginTop: '8px', width: '100px', fontWeight: 'bold', borderRadius: '24px' }}>
                                             <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7e57c2" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-image"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7e57c2" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="feather feather-image"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>
                                                 <span style={{ marginLeft: '4px' }}>Files</span>
                                             </Box>
                                         </Button>
-                                        <Button size="small" variant="contained" sx={{ marginTop: '8px', width: '100px', borderRadius: '24px' }}>Share</Button>
+                                        <Button disabled={!body} onClick={handleShare} size="small" variant="contained" sx={{ marginTop: '8px', width: '100px', borderRadius: '24px' }}>Share</Button>
                                     </Box>
                                 </Box>
                             </Box>
                         </Box>
-                        <Box sx={{ backgroundColor: '#181818', borderRadius: 2, p: 2 }}>
-
-                        </Box>
+                        <Grid container sx={{ backgroundColor: '#181818', borderRadius: 2, p: 2, gap: 1 }}>
+                            {
+                                posts.map((post) => (
+                                    <Post
+                                        key={post.id}
+                                        name={post.name}
+                                        body={post.body}
+                                    >
+                                    </Post>
+                                ))
+                            }
+                        </Grid>
                     </Grid>
                     <Grid item xs padding={'16px'}>
                         <Box sx={{ backgroundColor: '#181818', borderRadius: 2, p: 2 }}>
---
+++ src/pages/Post.jsx
@@ -0,0 +1,29 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+
+function Post({ name, body }) {
+
+    return (
+        <Card sx={{ width: '100%'}}>
+            <CardContent>
+                <Box sx={{ display: 'flex' }}>
+                    <Avatar src='https://cdn.nba.com/headshots/nba/latest/1040x760/445.png' />
+                    <Box sx={{ display: 'flex', flexDirection: 'column', marginLeft: '24px', width: '100%' }}>
+                        <Typography variant="subtitle1">{name}</Typography>
+                        <Typography variant="caption" color='grey'>Monday, May 16, 2024</Typography>
+
+                    </Box>
+                </Box>
+                <Box sx={{ marginTop: '16px' }}>
+                    <Typography variant="caption">{body}
+                    </Typography>
+                </Box>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Post;
