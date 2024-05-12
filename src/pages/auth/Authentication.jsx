6c7800d8989046d910f1046247983f2e7f83c92a
gelotuason <gelotuason@gmail.com>
2024-05-12 16:55:41 +0800

    removed lazy loading

--- src/pages/auth/Authentication.jsx
+++ src/pages/auth/Authentication.jsx
@@ -214,7 +214,6 @@ function Authentication() {
                         <img
                             src={'https://images.unsplash.com/photo-1528642474498-1af0c17fd8c3?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
                             alt={'People'}
-                            loading="lazy"
                             style={{ minWidth: '445px', maxWidth: '100%', height: 'auto', borderRadius: 6 }}
                         />
                     </Grid>
